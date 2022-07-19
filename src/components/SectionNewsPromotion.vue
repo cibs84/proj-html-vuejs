846fb1d80be34010dcc6e9ab60bbf5119f2e21df
cibs84 <d.tilesi@gmail.com>
2022-07-19 12:50:02 +0200

    SectionNewsPromotions hover effect

--- src/components/SectionNewsPromotion.vue
+++ src/components/SectionNewsPromotion.vue
@@ -12,10 +12,20 @@
                 <!-- Card -->
                 <div class="ms_card">
                     <img src="../assets/img/blog92x-600x687.jpg" alt="">
-                    <h3 class="title">Braised Abalone</h3>
-                    <div class="meta-data">
-                        By <a href="#">admin</a> | January 7th, 2020 | Categories: <a href="#">News</a>
+                    <div class="text-area">
+                        <h3 class="title">Braised Abalone</h3>
+                        <div class="meta-data">
+                            By <a href="#">admin</a> | January 7th, 2020 | Categories: <a href="#">News</a>
+                        </div>
                     </div>
+
+                    <!-- Card Hover Items -->
+                    <div class="ms_overlay">
+                        <span class="link-icon"><i class="fa-solid fa-link"></i></span>
+                        <h3 class="title">The best coffee in town</h3>
+                        <div class="meta-data">News</div>
+                    </div>
+                    <div class="hiding-element"></div>
                 </div>
             </div>
             <!-- Column -->
@@ -23,8 +33,20 @@
                 <!-- Card -->
                 <div class="ms_card">
                     <img src="../assets/img/blog72x-600x687.jpg" alt="">
-                    <h3 class="title">Braised Abalone</h3>
-                    <div class="meta-data">By <a href="#">admin</a> | January 7th, 2020 | Categories: <a href="#">News</a></div>
+                    <div class="text-area">
+                        <h3 class="title">Braised Abalone</h3>
+                        <div class="meta-data">
+                            By <a href="#">admin</a> | January 7th, 2020 | Categories: <a href="#">News</a>
+                        </div>
+                    </div>
+                    
+                    <!-- Card Hover Items -->
+                    <div class="ms_overlay">
+                        <span class="link-icon"><i class="fa-solid fa-link"></i></span>
+                        <h3 class="title">The best coffee in town</h3>
+                        <div class="meta-data">News</div>
+                    </div>
+                    <div class="hiding-element"></div>
                 </div>
             </div>
             <!-- Column -->
@@ -32,8 +54,20 @@
                 <!-- Card -->
                 <div class="ms_card">
                     <img src="../assets/img/blog82x-600x687.jpg" alt="">
-                    <h3 class="title">Braised Abalone</h3>
-                    <div class="meta-data">By <a href="#">admin</a> | January 7th, 2020 | Categories: <a href="#">News</a></div>
+                    <div class="text-area">
+                        <h3 class="title">Braised Abalone</h3>
+                        <div class="meta-data">
+                            By <a href="#">admin</a> | January 7th, 2020 | Categories: <a href="#">News</a>
+                        </div>
+                    </div>
+                
+                    <!-- Card Hover Items -->
+                    <div class="ms_overlay">
+                        <span class="link-icon"><i class="fa-solid fa-link"></i></span>
+                        <h3 class="title">The best coffee in town</h3>
+                        <div class="meta-data">News</div>
+                    </div>
+                    <div class="hiding-element"></div>
                 </div>
             </div>
             
@@ -56,6 +90,7 @@ export default {
 
 .news-promotions.section-C {
     padding: 8rem 0;
+    height: 70rem;
 
     .ms_secondary_container {
 
@@ -66,12 +101,14 @@ export default {
                 align-items: center;
                 color: $ms_black;
                 text-align: center;
+                position: relative;
+                height: 45rem;
+                cursor: pointer;
 
                 img {
-                    max-width: 100%;
-                    margin-bottom: 1rem;
-                    height: 40rem;
+                    height: 100%;
                     object-fit: cover;
+                    transition: all 0.2s;
                 }
                 .title {
                     font-size: 2rem;
@@ -88,6 +125,56 @@ export default {
                         &:hover {
                             text-decoration: underline;
                         }
+                    }
+                }
+                .text-area {
+                        position: absolute;
+                        top: calc(100% - 6rem);
+                        bottom: 0;
+                        left: 0;
+                        right: 0;
+                        background-color: #fff;
+                        transition: all 0.2s;
+                        z-index: 10;
+                        padding: 2rem 0;
+                }
+                .ms_overlay {
+                    background: linear-gradient(#000, #0000);
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    z-index: 30;
+                    color: white;
+                    opacity: 0;
+                    transition: all 0.2s;
+
+                    .link-icon {
+                        font-size: 1.3rem;
+                        margin-bottom: 0.2rem;
+                    }
+                }
+                .hiding-element {
+                        top: calc(100% - 0rem);
+                        position: absolute;
+                        top: 100%;
+                        bottom: 0;
+                        left: 0;
+                        right: 0;
+                        background-color: #fff;
+                        transition: all 0.2s;
+                        z-index: 20;
+                }
+                &:hover {
+
+                    .text-area {
+                        top: 100%;
+                    }
+                    .ms_overlay {
+                        opacity: 1;
+                    }
+                    .hiding-element {
+                        bottom: -6rem;
                     }
                 }
             }
